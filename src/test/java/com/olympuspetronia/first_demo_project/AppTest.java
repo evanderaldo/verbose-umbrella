dea4b1108b5604bc0ba5dcf4b4501e5bc2220b63
evanderaldo <evander.geraldo@gmail.com>
2023-03-08 21:57:35 -0600

    updated pom files

---
+++ src/test/java/com/olympuspetronia/first_demo_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.olympuspetronia.first_demo_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
